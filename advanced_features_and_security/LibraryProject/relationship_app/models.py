c4ab6f361c8e6da4dcf2667ce9c9329b7487a925
JohnyEmil <jonyjony7030@gmail.com>
2025-07-27 17:24:29 +0300

    the first task

---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    # Remove username field and make email the primary identifier
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
+    profile_photo = models.ImageField(_('profile photo'), upload_to='profile_photos/', null=True, blank=True)
+    
+    # Use email as the username field
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+    
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    class Role(models.TextChoices):
+        ADMIN = 'Admin'
+        LIBRARIAN = 'Librarian'
+        MEMBER = 'Member'
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.MEMBER)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.role}"
